ef6acb344d5a86c890927c523ba8bc5129cb4eac
Mikoláš Stuchlík <mikolas.stuchlik@thefuntasty.com>
2019-07-22 15:13:21 +0200

    Setup C library with sources

---
+++ CswiftLib/Sources/CswiftLib/c_code.c
@@ -0,0 +1,6 @@
+#include "include/c_code.h"
+#include <stdio.h>
+
+void print_hello_world() {
+    printf("Hello world!");
+}
---
+++ CswiftLib/Sources/CswiftLib/include/c_code.h
@@ -0,0 +1,6 @@
+#ifndef C_SWIFT_LIB_H
+#define C_SWIFT_LIB_H
+
+void print_hello_world();
+
+#endif /* C_SWIFT_LIB_H */
